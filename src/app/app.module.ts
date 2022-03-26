b878e0237d162498d7ed734f8de411614cd724c2
gamaliel <gamaliel.aq96@gmail.com>
2022-03-26 01:50:53 -0500

    Configuracion StoreDevtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
+import { environment } from './../environments/environment.prod';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+//NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { contadorReducer } from './contador/contador.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +20,13 @@ import { NietoComponent } from './contador/nieto/nieto.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({contador: contadorReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nieto',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nieto.component.scss']
 })
 export class NietoComponent implements OnInit {
-
+  @Input() contador: number;
+  @Output() contadorCambio = new EventEmitter<number>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  reset() {
+    this.contador = 0;
+    this.contadorCambio.emit(this.contador);
+  }
 }
