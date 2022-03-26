894bd1a071bd9b0048455cbc6e2db7574ec1528c
gamaliel <gamaliel.aq96@gmail.com>
2022-03-26 00:56:08 -0500

    Contador

---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.scss']
+})
+export class HijoComponent implements OnInit {
+  @Input() contador: number = 0;
+  @Output() cambioContador = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  multiplicar() {
+    this.contador *= 2;
+    this.cambioContador.emit(this.contador)
+  }
+
+  dividir() {
+    this.contador /= 2;
+    this.cambioContador.emit(this.contador)
+  }
+}
